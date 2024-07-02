09d3d9b4439f42026ac0ef3db1afe0d800f3394f
kaohaohan <haohan920@gmail.com>
2024-07-02 17:49:22 -0400

    change css route

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const path = require("path");
 const app = express();
 const port = 3001;
 
-app.set("views", __dirname + "/views");
+app.use(express.static(`${__dirname}/public`));
+app.set("views", path.join(__dirname) + "/views");
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
 
 // 初始化 LegoSets 模块
 legoSets.initialize();
